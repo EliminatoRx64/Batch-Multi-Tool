ad04dbb14ccf09e21cca0b61998c175487f397eb
EliminatoRx64 <hierl.elias@gmail.com>
2025-01-26 15:48:54 +0100

    main.bat final

--- multitool/main.bat
+++ multitool/main.bat
@@ -1,5 +1,5 @@
 @echo off
-title MultiTool - by Ebola Man
+title MultiToolRpi - stolen from Ebola Man by EliHeli999
 chcp 65001 >nul
 cd files
 color 5
@@ -10,18 +10,18 @@ call :banner
 for /f %%A in ('"prompt $H &echo on &for %%B in (1) do rem"') do set BS=%%A
 echo.
 echo.
-echo [38;2;255;255;0m        ╔═(1) Process Hacker 2[0m  
-echo [38;2;255;255;0m        ║[0m  
-echo [38;2;255;255;0m        ╠══(2) Wireshark[0m  
-echo [38;2;255;255;0m        ║[0m  
-echo [38;2;255;255;0m        ╠═══(3) Virtualbox[0m  
-echo [38;2;255;255;0m        ║[0m  
-echo [38;2;255;255;0m        ╚╦═══(4) Putty[0m  
-echo [38;2;255;255;0m         ║[0m  
-set /p input=.%BS% [38;2;255;255;0m        ╚══════^>[0m  
-if /I %input% EQU 1 start ph2.lnk
-if /I %input% EQU 2 start Wireshark.lnk
-if /I %input% EQU 3 start vbox.lnk
+echo [38;2;0;0;255m        ╔═(1) Raspberry Pi Imager [0m  
+echo [38;2;0;0;255m        ║[0m  
+echo [38;2;0;0;255m        ╠══(2) WinSCP[0m  
+echo [38;2;0;0;255m        ║[0m  
+echo [38;2;0;0;255m        ╠═══(3) Notepad++[0m  
+echo [38;2;0;0;255m        ║[0m  
+echo [38;2;0;0;255m        ╚╦═══(4) Putty[0m  
+echo [38;2;0;0;255m         ║[0m  
+set /p input=.%BS% [38;2;0;0;255m        ╚══════^>[0m  
+if /I %input% EQU 1 start imgr.lnk
+if /I %input% EQU 2 start WinSCP.lnk
+if /I %input% EQU 3 start Notepad++.lnk
 if /I %input% EQU 4 start putty.exe
 cls
 goto start
@@ -29,10 +29,10 @@ goto start
 :banner
 echo.
 echo.
-echo                     [38;2;255;0;0m███╗   ███╗██╗   ██╗██╗  ████████╗██╗    ████████╗ ██████╗  ██████╗ ██╗[0m     
-echo                     [38;2;255;51;0m████╗ ████║██║   ██║██║  ╚══██╔══╝██║    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     [0m
-echo                     [38;2;255;102;0m██╔████╔██║██║   ██║██║     ██║   ██║       ██║   ██║   ██║██║   ██║██║    [0m 
-echo                     [38;2;255;153;0m██║╚██╔╝██║██║   ██║██║     ██║   ██║       ██║   ██║   ██║██║   ██║██║     [0m
-echo                     [38;2;255;204;0m██║ ╚═╝ ██║╚██████╔╝███████╗██║   ██║       ██║   ╚██████╔╝╚██████╔╝███████╗[0m
-echo                     [38;2;255;255;0m╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝[0m
+echo                     [38;2;255;0;255m███╗   ███╗██╗   ██╗██╗  ████████╗██╗    ████████╗ ██████╗  ██████╗ ██╗[0m     
+echo                     [38;2;205;0;255m████╗ ████║██║   ██║██║  ╚══██╔══╝██║    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     [0m
+echo                     [38;2;155;0;255m██╔████╔██║██║   ██║██║     ██║   ██║       ██║   ██║   ██║██║   ██║██║    [0m 
+echo                     [38;2;105;0;255m██║╚██╔╝██║██║   ██║██║     ██║   ██║       ██║   ██║   ██║██║   ██║██║     [0m
+echo                     [38;2;55;0;255m██║ ╚═╝ ██║╚██████╔╝███████╗██║   ██║       ██║   ╚██████╔╝╚██████╔╝███████╗[0m
+echo                     [38;2;0;0;255m╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝[0m
 echo.
